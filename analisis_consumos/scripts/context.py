81885f135b199d6fac7309ea0520614c0a6b55a5
Juan Hernández <jjhernandezporras@gmail.com>
2025-03-28 22:34:07 +0100

    consumption clustering script

---
+++ analisis_consumos/scripts/context.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+RANDOM_STATE = 42
+TIME_ZONE = "Europe/Madrid"
+DATA_DIR = Path('../data')
---
+++ analisis_consumos/scripts/energy_consumption_groups.py
@@ -0,0 +1,11 @@
+from context import RANDOM_STATE
+from context import TIME_ZONE
+from tfm.feature_engineering.cyclical_encoding import add_sine_cosine_transformation
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
