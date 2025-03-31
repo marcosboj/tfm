235d2ae15c7c4cc6666788d8515834c1575986cc
Juan Hernández <jjhernandezporras@gmail.com>
2025-03-31 10:10:05 +0200

    updated script with new info from dummy data

--- analisis_consumos/scripts/energy_consumption_groups.py
+++ analisis_consumos/scripts/energy_consumption_groups.py
@@ -10,21 +10,35 @@ from tfm.feature_engineering.cyclical_encoding import add_sine_cosine_transforma
 from tfm.utils.data_frame import add_timestamp_values
 
 
-def main(csv_file: Path):
-    df = pd.read_csv(csv_file)
-    df["datetime"] = pd.to_datetime(df["datetime"], utc=True).dt.tz_convert(TIME_ZONE)
-    print(df)
+def process_timestamp(df: pd.DataFrame, tz: str) -> pd.DataFrame:
+    modified_df = df.copy()
+    modified_df["datetime"] = pd.to_datetime(df["datetime"], utc=True).dt.tz_convert(tz=tz)
 
-    # Add time-related features
-    df = add_timestamp_values(df, "datetime")
-    df = add_sine_cosine_transformation(df, "hour", 24)
+    return modified_df
+
+
+def main(csv_file: Path):
+    df = pd.read_csv(
+        csv_file,
+        dtype={'nif': str}
+    )
+    if "datetime" in df.columns:
+        df = process_timestamp(df, TIME_ZONE)
+        # Add time-related features
+        df = add_timestamp_values(df, "datetime")
+        df = add_sine_cosine_transformation(df, "hour", 24)
 
     # Clustering
-    x = df[["a", "b", "hour", "hour_sin", "hour_cos"]].values
-    kmeans = KMeans(n_clusters=2, random_state=RANDOM_STATE, n_init="auto")
+    features = [
+        "mean_consumption", "std_consumption", "min_consumption", "max_consumption"
+    ]
+    x = df[features].values
+    kmeans = KMeans(n_clusters=2, random_state=RANDOM_STATE)
     kmeans.fit(x)
     df["cluster"] = kmeans.predict(x)
-    print(df[["datetime", "a", "b", "cluster"]])
+    groups = df.groupby(by="cluster")['nif'].unique()
+    for cluster, value in groups.items():
+        print(f"Cluster {cluster}: {", ".join(value)}")
 
 
 if __name__ == "__main__":
